e489d0e78a680400a9adfe7af641d217e9724c11
kogorkus <kogorkus@mail.ru>
2018-11-24 15:47:43 +0300

    1

---
+++ Test/Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    delegate void Del();
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Del MyDel = DoSmth;
+            MyDel += DoSmthElse;
+            MyDel();
+        }
+        public static void DoSmth()
+        {
+            Console.WriteLine("Smth`s done!");
+        }
+        public static void DoSmthElse()
+        {
+            Console.WriteLine("Smth else is done too!");
+        }
+    }
+
+}
+
